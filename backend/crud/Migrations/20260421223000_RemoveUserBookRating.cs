d3c7f799850bf0106e9ac4966d7d4e89490d564e
Shifdis <huaweitit0101@gmail.com>
2026-04-22 00:14:21 +0300

    fix migration discovery for removing

--- backend/crud/Migrations/20260421223000_RemoveUserBookRating.cs
+++ backend/crud/Migrations/20260421223000_RemoveUserBookRating.cs
@@ -1,9 +1,13 @@
 using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using crud.Data;
 
 #nullable disable
 
 namespace crud.Migrations;
 
+[DbContext(typeof(CrudDbContext))]
+[Migration("20260421223000_RemoveUserBookRating")]
 public partial class RemoveUserBookRating : Migration
 {
     protected override void Up(MigrationBuilder migrationBuilder)
