481b0a9062fb9bc95966fa07724e5843833e20ba
Anna <Anna.rm07@mail.ru>
2026-04-22 15:54:58 +0300

    favourites fixed

--- frontend/app/slices/userSlice.js
+++ frontend/app/slices/userSlice.js
@@ -24,7 +24,7 @@ const baseQueryWithReauth = async (args, api, extraOptions) => {
 export const userApi = createApi({
   reducerPath: "userApi",
   baseQuery: baseQueryWithReauth,
-  tagTypes: ["User", "Books", "RecommendedBooks"],
+  tagTypes: ["User", "Books", "RecommendedBooks", "Post", "PostComment", "FavoritePost"],
   endpoints: (builder) => ({
     registerUser: builder.mutation({
       query: (body) => ({
